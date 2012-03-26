b58c02d14c8ee005d4aad0452255cc486c754bb7
salonean <salonean@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-03-26 11:22:02 +0000

    small build fixes

--- generic-test-support/src/main/java/fi/vm/sade/koodisto/selenium/SeleniumTestCaseSupport.java
+++ generic-test-support/src/main/java/fi/vm/sade/koodisto/selenium/SeleniumTestCaseSupport.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class SeleniumTestCaseSupport extends TestCase {
 
+    public static final int TIME_OUT_IN_SECONDS = 10;
+    public static final int SLEEP_IN_MILLIS = 3000;
     protected WebDriver driver;
     protected String ophServerUrl = "http://localhost";
 
@@ -18,7 +20,7 @@ public abstract class SeleniumTestCaseSupport extends TestCase {
         } catch (Exception e) {
             System.out.println("selenium failed to initialize firefox, falling back to htmlunit");
             driver = new HtmlUnitDriver();
-            ((HtmlUnitDriver)driver).setJavascriptEnabled(true);
+            ((HtmlUnitDriver) driver).setJavascriptEnabled(true);
         }
         
         if (System.getenv("OPH_SERVER_URL") != null) {
@@ -27,7 +29,7 @@ public abstract class SeleniumTestCaseSupport extends TestCase {
         if (System.getProperty("ophServerUrl") != null) {
             ophServerUrl = System.getProperty("ophServerUrl");
         }
-        System.out.println("selenium using ophServerUrl: "+ophServerUrl);
+        System.out.println("selenium using ophServerUrl: " + ophServerUrl);
     }
 
     @Override
@@ -37,12 +39,12 @@ public abstract class SeleniumTestCaseSupport extends TestCase {
     }
 
     protected void waitForPageSourceContains(final String relativeUrl, final String expectedContains) {
-        (new WebDriverWait(driver, 10, 3)).until(new ExpectedCondition<Boolean>() {
+        (new WebDriverWait(driver, TIME_OUT_IN_SECONDS, SLEEP_IN_MILLIS)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
-                String url = ophServerUrl+ relativeUrl;
+                String url = ophServerUrl + relativeUrl;
                 driver.get(url);
                 boolean contains = driver.getPageSource().contains(expectedContains);
-                System.out.println(this.getClass().getSimpleName()+" - url: "+url+", expectedContains: "+expectedContains+", contains: "+contains);
+                System.out.println(this.getClass().getSimpleName() + " - url: " + url + ", expectedContains: " + expectedContains + ", contains: " + contains);
 //                if (!contains) {
 //                    System.out.println(this.getClass().getSimpleName()+" - page source: "+driver.getPageSource());
 //                }
