8bbf4532f3c519be290a6e20f3751e26c1836ad3
salonean <salonean@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-04-27 08:06:45 +0000

    OVT-453 poikkeukset servicetasolta ja niiden lokalisointi

---
+++ generic-common/src/main/java/fi/vm/sade/generic/common/LocalizedBusinessException.java
@@ -0,0 +1,37 @@
+package fi.vm.sade.generic.common;
+
+/**
+ * @author Antti Salonen
+ */
+public class LocalizedBusinessException extends Exception {
+
+    private String key;
+
+    public LocalizedBusinessException(String key) {
+        super(key);
+        this.key = key;
+    }
+
+    public LocalizedBusinessException(String message, String key) {
+        super(message);
+        this.key = key;
+    }
+
+    public LocalizedBusinessException(String message, Throwable cause, String key) {
+        super(message, cause);
+        this.key = key;
+    }
+
+    public LocalizedBusinessException(Throwable cause, String key) {
+        super(cause);
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
--- generic-common/src/main/java/fi/vm/sade/generic/common/ValidationException.java
+++ generic-common/src/main/java/fi/vm/sade/generic/common/ValidationException.java
@@ -6,20 +6,27 @@ import java.util.List;
 /**
  * @author Antti
  */
-public class ValidationException extends Exception {
+public class ValidationException extends LocalizedBusinessException {
 
+    public static final String KEY = "validation.exception";
     private List<String> validationMessages = new ArrayList<String>();
 
     public ValidationException() {
+        super(KEY);
     }
 
     public ValidationException(String message) {
+        super(KEY);
         addValidationMessage(message);
     }
 
     public ValidationException(String message, Exception cause) {
-        super(cause);
+        super(cause, KEY);
         addValidationMessage(message);
+    }
+
+    public ValidationException(String message, String key) {
+        super(message, key);
     }
 
     public void addValidationMessage(String msg) {
