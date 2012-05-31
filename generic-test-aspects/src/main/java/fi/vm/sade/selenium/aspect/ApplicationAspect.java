2172100ba7afe4bcf50bfdf7c6e063f2ce71cd96
hannilto <hannilto@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-05-31 09:23:04 +0000

    Korjattu nullpointer, kun ei-testimoodissa.

--- generic-test-aspects/src/main/java/fi/vm/sade/selenium/aspect/ApplicationAspect.java
+++ generic-test-aspects/src/main/java/fi/vm/sade/selenium/aspect/ApplicationAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.Application;
@@ -18,15 +19,20 @@ import com.vaadin.ui.Component;
  *
  */
 @Aspect
-public class ApplicationAspect {
+public class ApplicationAspect implements InitializingBean {
 
     @Autowired
     private ComponentCatcher catcher;
+    
+    private boolean initialized = false;
     
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     @After("execution(com.vaadin.Application+.new(..))")
     public void afterApplicationConstruct(JoinPoint pjp) throws Throwable {
+        if(!initialized) {
+            return;
+        }
         logger.debug("Invoked after Application: " + pjp.getTarget());
         Application application = (Application) pjp.getTarget();
         catcher.execute(application);
@@ -34,6 +40,9 @@ public class ApplicationAspect {
     
     @After("execution(com.vaadin.ui.Component+.new(..))")
     public void afterComponentConstruct(JoinPoint pjp) throws Throwable {
+        if(!initialized) {
+            return;
+        }
         logger.debug("Invoked after Component: " + pjp.getTarget());
         Component component = (Component) pjp.getTarget();
         catcher.execute(component);
@@ -46,4 +55,10 @@ public class ApplicationAspect {
     public void setCatcher(ComponentCatcher catcher) {
         this.catcher = catcher;
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.initialized = true;
+    }
+
 }
