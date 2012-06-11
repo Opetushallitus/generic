0d878e2e38748e0d805e92b8974aaebed07d7b6f
kkammonen <kkammonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-06-11 13:57:02 +0000

    date helper

---
+++ generic-common/src/main/java/fi/vm/sade/generic/common/DateHelper.java
@@ -0,0 +1,47 @@
+package fi.vm.sade.generic.common;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public final class DateHelper {
+
+    private static SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy HH:mm");
+
+    public static String xmlCalToTypicalString(XMLGregorianCalendar cal) {
+        if (cal != null) {
+            Date date = xmlCalToDate(cal);
+            return dateToTypicalString(date);
+        }
+        return "";
+    }
+
+    public static String dateToTypicalString(Date date) {
+        if (date != null) {
+            return formatter.format(date);
+        }
+        return "";
+    }
+
+    public static Date xmlCalToDate(XMLGregorianCalendar cal) {
+        if (cal != null) {
+            return cal.toGregorianCalendar().getTime();
+        }
+        return null;
+    }
+
+    public static XMLGregorianCalendar DateToXmlCal(Date date) {
+        try {
+            GregorianCalendar gcal = new GregorianCalendar();
+            gcal.setTime(date);
+            XMLGregorianCalendar xgcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
+            return xgcal;
+        } catch (DatatypeConfigurationException e) {
+            throw new RuntimeException("Operation with XMLGregorianCalendar failed", e);
+        }
+    }
+}
