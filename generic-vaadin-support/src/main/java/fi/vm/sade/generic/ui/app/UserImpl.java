9d5c5234bf66d89581fe15b22d66f83954ace2ab
jkaistinen <jkaistinen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-08-21 09:50:08 +0000

    OVT-1321: Haetaan oid liferay käyttäjältä.

--- generic-vaadin-support/src/main/java/fi/vm/sade/generic/ui/app/UserImpl.java
+++ generic-vaadin-support/src/main/java/fi/vm/sade/generic/ui/app/UserImpl.java
@@ -5,35 +5,44 @@ import java.util.List;
 
 import javax.portlet.PortletRequest;
 
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+
 import fi.vm.sade.generic.ui.portlet.security.AccessRight;
 import fi.vm.sade.generic.ui.portlet.security.User;
 
 /**
- * Consider writing this impl in different way that does not stash {@link PortletRequest}
+ * Consider writing this impl in different way that does not stash
+ * {@link PortletRequest}
+ * 
  * @author kkammone
- *
+ * 
  */
 public class UserImpl implements User {
 
     private PortletRequest r;
-    
+
     public UserImpl(PortletRequest portletRequest) {
-       this.r = portletRequest;
+        this.r = portletRequest;
     }
-    
+
     @Override
     public boolean isUserInRole(String role) {
-       return r.isUserInRole(role);
+        return r.isUserInRole(role);
     }
 
     @Override
     public String getOid() {
-        return "NOT IMPLEMENTED";
+        ThemeDisplay themeDisplay = (ThemeDisplay) r.getAttribute(WebKeys.THEME_DISPLAY);
+
+        com.liferay.portal.model.User liferayUser = themeDisplay.getUser();
+        String attribute = (String) liferayUser.getExpandoBridge().getAttribute("oid_henkilo");
+
+        return attribute;
     }
 
     @Override
     public List<AccessRight> getRawAccessRights() {
-       return new ArrayList<AccessRight>();
+        return new ArrayList<AccessRight>();
     }
-
 }
