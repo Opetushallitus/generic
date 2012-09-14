822df95f850a70a54029dc358a56cca256238c49
eblomqvist <eblomqvist@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-14 09:42:58 +0000

    Mock implementation for ticket callback.

---
+++ generic-portlet-support/src/main/java/fi/vm/sade/generic/ui/portlet/security/mock/MockTicketCallback.java
@@ -0,0 +1,20 @@
+package fi.vm.sade.generic.ui.portlet.security.mock;
+
+import fi.vm.sade.generic.common.auth.xml.TicketHeader;
+import fi.vm.sade.generic.ui.portlet.security.SecurityTicketCallback;
+
+/**
+ * Mock implementation of ticket callback.
+ *
+ * @author Eetu Blomqvist
+ */
+public class MockTicketCallback implements SecurityTicketCallback {
+
+    @Override
+    public TicketHeader getTicketHeader() {
+        TicketHeader ticketHeader = new TicketHeader();
+        ticketHeader.username = "1.2.246.562.24.00000000001";
+        ticketHeader.ticket = "lc8925kjgi12nfd91";
+        return ticketHeader;
+    }
+}
