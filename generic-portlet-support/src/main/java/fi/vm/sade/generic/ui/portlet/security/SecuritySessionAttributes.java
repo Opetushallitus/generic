fe31a41e37717ff2fdef113373daf830f8885e1a
kkammonen <kkammonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-18 12:54:16 +0000

    ticket support

--- generic-portlet-support/src/main/java/fi/vm/sade/generic/ui/portlet/security/SecuritySessionAttributes.java
+++ generic-portlet-support/src/main/java/fi/vm/sade/generic/ui/portlet/security/SecuritySessionAttributes.java
@@ -1,9 +1,18 @@
 package fi.vm.sade.generic.ui.portlet.security;
 
 public class SecuritySessionAttributes {
+    
+    /**
+     * Liferay requires this so that session scope is shared from servlet to portlets (and between WARs) 
+     */
+    public static final String LIFERAY_SESSION_SHARE_PREFIX = "LIFERAY_SHARED_"; 
 
-    public static final String AUTHENTICATION_DATA = "AUTHENTICATION_DATA";
+    public static final String AUTHENTICATION_DATA =  LIFERAY_SESSION_SHARE_PREFIX + "AUTHENTICATION_DATA";
 
-    public static final String TICKET = "TICKET";
+    public static final String TICKET = LIFERAY_SESSION_SHARE_PREFIX +  "TICKET";
+    
+   
+    
+    
 
 }
