42f58737c285d9a3006c33b436d8fca8d9c3832d
hannilto <hannilto@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-11 13:32:07 +0000

    Koodiston download and upload -toiminnot hajoavat soap headerien puuttumiseen.

--- generic-esb/src/main/java/fi/vm/sade/generic/esb/processor/SoapHeaderDelegatingProcessor.java
+++ generic-esb/src/main/java/fi/vm/sade/generic/esb/processor/SoapHeaderDelegatingProcessor.java
@@ -7,6 +7,7 @@ import org.apache.cxf.binding.soap.SoapHeader;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.headers.Header;
+import org.apache.cxf.message.MessageContentsList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -25,11 +26,12 @@ public class SoapHeaderDelegatingProcessor implements Processor, InitializingBea
 
     private static final Logger logger = LoggerFactory.getLogger(SoapHeaderDelegatingProcessor.class);
 
-    private Object client;
+    private final List<Object> clients = new ArrayList<Object>();
     private List<QName> qNames;
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        // Payload mode
         if(exchange.getIn().getBody() instanceof CxfPayload) {
             CxfPayload<SoapHeader> payload = exchange.getIn().getBody(CxfPayload.class);
             List<SoapHeader> invokeHeaders = new ArrayList<SoapHeader>();
@@ -39,20 +41,43 @@ public class SoapHeaderDelegatingProcessor implements Processor, InitializingBea
                 }
             }
 
-            Client proxy = ClientProxy.getClient(this.client);
-            proxy.getRequestContext().put("thread.local.request.context", "true");
-            proxy.getRequestContext().put(Header.HEADER_LIST, invokeHeaders);
+            addHeadersToAllClients(invokeHeaders);
+        // POJO mode
+        } else if(exchange.getIn().getBody() instanceof MessageContentsList) {
+            MessageContentsList list = exchange.getIn().getBody(MessageContentsList.class);
+            List<SoapHeader> soapHeaders = exchange.getIn().getHeader(Header.HEADER_LIST, List.class);
+            List<SoapHeader> invokeHeaders = new ArrayList<SoapHeader>();
+            for(SoapHeader header : soapHeaders) {
+                if(qNames.contains(header.getName())) {
+                    invokeHeaders.add(new SoapHeader(header.getName(), header.getObject()));
+                }
+            }
+
+            addHeadersToAllClients(invokeHeaders);
         } else {
-            logger.warn("Couldn't use the processor because body was not CxfPayload.");
+            logger.warn("Couldn't use the processor because body was not CxfPayload or MessageContentsList.");
         }
     }
 
-    public Object getClient() {
-        return client;
+    private void addHeadersToAllClients(List<SoapHeader> soapHeaders) {
+        for(Object client : this.clients) {
+            try {
+                Client proxy = ClientProxy.getClient(client);
+                proxy.getRequestContext().put("thread.local.request.context", "true");
+                proxy.getRequestContext().put(Header.HEADER_LIST, soapHeaders);
+            } catch (Exception e) {
+                // Most likely not client proxy.
+                logger.warn("Header delegating for client " + client + " of type " + (client != null ? client.getClass().getName() : null) + " failed", e);
+            }
+        }
     }
 
     public void setClient(Object client) {
-        this.client = client;
+        this.clients.add(client);
+    }
+
+    public void setClients(List<Object> clients) {
+        this.clients.addAll(clients);
     }
 
     public List<QName> getqNames() {
@@ -65,7 +90,7 @@ public class SoapHeaderDelegatingProcessor implements Processor, InitializingBea
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        Assert.notNull(client, "You must set CXF client proxy to the processor");
+        Assert.notNull(clients.isEmpty(), "You must set CXF client proxy to the processor");
         Assert.notNull(qNames, "List of delegated qnames is null.");
     }
 }
