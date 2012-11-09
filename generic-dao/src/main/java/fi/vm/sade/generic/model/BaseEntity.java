60e50610253692bcc0210800f242e96161836759
mlyly <mlyly@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-09 10:46:22 +0000

    OVT-2793 Versiointia organisaatioon, lisätty kommentteja ja perus toString-representaatio

--- generic-dao/src/main/java/fi/vm/sade/generic/model/BaseEntity.java
+++ generic-dao/src/main/java/fi/vm/sade/generic/model/BaseEntity.java
@@ -1,5 +1,18 @@
-/**
+/*
  *
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
  */
 package fi.vm.sade.generic.model;
 
@@ -12,14 +25,15 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
 /**
+ * Base class for entities in the system.
+ *
+ * Support for versioning with optimistic locking.
+ *
  * @author tommiha
  */
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = -1482830143396044915L;
 
     public static final String ID_COLUMN_NAME = "id";
@@ -54,19 +68,25 @@ public class BaseEntity implements Serializable {
     public boolean equals(Object o) {
         if (this == o) {
             return true;
-
         }
-        // XXX: eblomqvist: removed - direct dependency to Hibernate
-        // if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
-        // return false;
-        // }
-
         return o instanceof BaseEntity && id != null && id.equals(((BaseEntity) o).getId());
-
     }
 
     @Override
     public int hashCode() {
         return id == null ? super.hashCode() : id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.getClass().getSimpleName());
+        sb.append("[id=");
+        sb.append(getId());
+        sb.append(", version=");
+        sb.append(getVersion());
+        sb.append("]");
+
+        return sb.toString();
     }
 }
