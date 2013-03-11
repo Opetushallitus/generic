5ab33034a265cfde60177b4a0eabed5a73077d6f
salonean <salonean@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-11 20:02:38 +0000

    cas liferay-auth-impl siivousta ja refaktorointia

--- generic-security/src/main/java/fi/vm/sade/security/TraceInterceptor.java
+++ generic-security/src/main/java/fi/vm/sade/security/TraceInterceptor.java
@@ -1,26 +1,44 @@
 package fi.vm.sade.security;
 
+import fi.vm.sade.generic.ui.feature.UserFeature;
+import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.logging.Log;
 import org.springframework.aop.interceptor.CustomizableTraceInterceptor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * Poor man's performance tracer, investigate Spring etc performance interceptors if this are used beyond temp usage
+ *
  * @author Antti Salonen
  */
-public class TraceInterceptor extends CustomizableTraceInterceptor {
+public class TraceInterceptor implements MethodInterceptor {
 
-    protected void writeToLog(Log logger, String message, Throwable ex) {
-//        if (ex != null) {
-//            logger.info(message, ex);
-//        } else {
-//            logger.info(message);
-//        }
-        System.out.println("TRACE - "+message+(ex!=null?" ("+ex+")":""));
+    private static Map<String, Long> cumulativeInvocationTimes = new HashMap<String, Long>();
+
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        long t0 = System.currentTimeMillis();
+        try {
+            return methodInvocation.proceed();
+        } finally {
+            long invocationTime = System.currentTimeMillis() - t0;
+            String methodKey = methodInvocation.getMethod().getDeclaringClass().getSimpleName()+"."+methodInvocation.getMethod().getName();
+            Long cumulativeTime = addCumulativeTime(invocationTime, methodKey);
+            System.out.println("PERFORMANCE TRACE, method: "+methodKey+", cumulative time (ms): "+cumulativeTime);
+        }
     }
 
-
-    protected boolean isInterceptorEnabled(MethodInvocation invocation, Log logger) {
-        return true;
+    private synchronized static Long addCumulativeTime(long invocationTime, String methodKey) {
+        Long cumulativeInvocationTime = cumulativeInvocationTimes.get(methodKey);
+        if (cumulativeInvocationTime == null) {
+            cumulativeInvocationTime = 0l;
+        }
+        cumulativeInvocationTime += invocationTime;
+        cumulativeInvocationTimes.put(methodKey, cumulativeInvocationTime);
+        return cumulativeInvocationTime;
     }
 
 }
