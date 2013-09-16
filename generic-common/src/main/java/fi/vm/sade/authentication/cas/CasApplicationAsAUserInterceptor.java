9d752934b45a7169e9f3a0c20216d603f9d56641
Juho Kaistinen <jukais@gofore.com>
2013-09-16 14:52:56 +0300

    VT-510: Dev modeen käyttöoikeudet

--- generic-common/src/main/java/fi/vm/sade/authentication/cas/CasApplicationAsAUserInterceptor.java
+++ generic-common/src/main/java/fi/vm/sade/authentication/cas/CasApplicationAsAUserInterceptor.java
@@ -6,8 +6,18 @@ import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.phase.Phase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Interceptor for outgoing SOAP calls that uses "application-as-a-user" pattern: authenticates against CAS REST API to get a service ticket.
@@ -23,12 +33,34 @@ public class CasApplicationAsAUserInterceptor extends AbstractSoapInterceptor {
     private String appClientUsername;
     private String appClientPassword;
 
+    @Value("${auth.mode:cas}")
+    private String authMode;
+
     public CasApplicationAsAUserInterceptor() {
         super(Phase.PRE_PROTOCOL);
     }
 
     @Override
     public void handleMessage(SoapMessage message) throws Fault {
+        if ("dev".equals(authMode)) {
+
+            Set<GrantedAuthority> authorities = buildMockAuthorities();
+            // String mockUser = "admin@oph.fi";
+            String mockUser = "1.2.246.562.24.00000000001";
+            logger.warn("building mock user: " + mockUser + ", authorities: " + authorities);
+            Authentication authentication = new TestingAuthenticationToken(mockUser, mockUser, new ArrayList<GrantedAuthority>(
+                    authorities));
+            //initSupportForOldAuthzFromSpringAuthentication();
+
+            ((HttpURLConnection) message.get("http.connection")).setRequestProperty("CasSecurityTicket", "oldDeprecatedSecurity_REMOVE");
+            String user = authentication.getName();
+            //String authorities = toString(authentication.getAuthorities());
+            ((HttpURLConnection) message.get("http.connection")).setRequestProperty("oldDeprecatedSecurity_REMOVE_username", user);
+            ((HttpURLConnection) message.get("http.connection")).setRequestProperty("oldDeprecatedSecurity_REMOVE_authorities", toString(authorities));
+            logger.info("DEV Proxy ticket! user: "+ user + ", authorities: "+authorities);
+            return;
+        }
+
         // authenticate against CAS REST API, and get a service ticket
         String serviceTicket = CasClient.getTicket(webCasUrl + "/v1/tickets", appClientUsername, appClientPassword, targetService);
 
@@ -52,5 +84,34 @@ public class CasApplicationAsAUserInterceptor extends AbstractSoapInterceptor {
 
     public void setAppClientPassword(String appClientPassword) {
         this.appClientPassword = appClientPassword;
+    }
+
+    private String toString(Collection<? extends GrantedAuthority> authorities) {
+        StringBuffer sb = new StringBuffer();
+        for (GrantedAuthority authority : authorities) {
+            sb.append(authority.getAuthority()).append(",");
+        }
+        return sb.toString();
+    }
+
+    public static Set<GrantedAuthority> buildMockAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+        // String org = "1.2.246.562.10.10108401950"; // espoon kaupunki
+        String org = "1.2.246.562.10.00000000001"; // root
+        String apps[] = new String[] { "ANOMUSTENHALLINTA", "ORGANISAATIOHALLINTA", "HENKILONHALLINTA", "KOODISTO",
+                "KOOSTEROOLIENHALLINTA", "OID", "OMATTIEDOT", "ORGANISAATIOHALLINTA", "TARJONTA", "SIJOITTELU", "VALINTOJENTOTEUTTAMINEN", "VALINTAPERUSTEET" };
+        String roles[] = new String[] { "READ", "READ_UPDATE", "CRUD" };
+        for (String app : apps) {
+            for (String role : roles) {
+                GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_APP_" + app + "_" + role); // sama
+                // rooli
+                // ilman
+                // oidia
+                GrantedAuthority authorityOid = new SimpleGrantedAuthority("ROLE_APP_" + app + "_" + role + "_" + org);
+                authorities.add(authority);
+                authorities.add(authorityOid);
+            }
+        }
+        return authorities;
     }
 }
