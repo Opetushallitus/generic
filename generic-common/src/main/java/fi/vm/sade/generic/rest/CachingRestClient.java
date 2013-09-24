5eaaaee62863fa1bb44141b6ee62e5359bd4d10b
Tommi Hännikkälä <tommi.hannikkala@siili.fi>
2013-09-24 15:47:47 +0300

    ARK-234

--- generic-common/src/main/java/fi/vm/sade/generic/rest/CachingRestClient.java
+++ generic-common/src/main/java/fi/vm/sade/generic/rest/CachingRestClient.java
@@ -2,9 +2,11 @@ package fi.vm.sade.generic.rest;
 
 import com.google.gson.*;
 import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
+import fi.vm.sade.authentication.cas.CasClient;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.cache.CacheConfig;
 import org.apache.http.impl.client.cache.CachingHttpClient;
@@ -19,6 +21,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.net.URISyntaxException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -57,6 +60,12 @@ public class CachingRestClient {
     //private HttpResponse response;
     private Object cacheStatus;  //used in tests
     private Gson gson;
+
+    private String webCasUrl;
+    private String username;
+    private String password;
+    private String casService;
+    private String ticket;
 
     public CachingRestClient() {
         // multithread support + max connections
@@ -101,7 +110,9 @@ public class CachingRestClient {
             T t = gson.fromJson(new InputStreamReader(is, "utf-8"), resultType);
             return t;
         } finally {
-            is.close();
+            if(is != null) {
+                is.close();
+            }
         }
     }
 
@@ -111,8 +122,25 @@ public class CachingRestClient {
     public InputStream get(String url) throws IOException {
 //        logger.info("get... url: {}", url);
         final HttpGet httpget = new HttpGet(url);
+
+        if(webCasUrl != null && username != null && password != null && casService != null && ticket == null) {
+            ticket = CasClient.getTicket(webCasUrl + "/v1/tickets", username, password, casService);
+            URIBuilder builder = new URIBuilder(httpget.getURI()).addParameter("ticket", ticket);
+            try {
+                httpget.setURI(builder.build());
+            } catch (URISyntaxException e) {
+                logger.error("URI syntax incorrect." , e);
+            }
+        }
+
         final HttpResponse response = cachingClient.execute(httpget, localContext.get());
-cacheStatus = localContext.get().getAttribute(CachingHttpClient.CACHE_RESPONSE_STATUS);
+
+        if(response.getStatusLine().getStatusCode() == 401) {
+            logger.warn("Wrong status code 401, clearing ticket.", response.getStatusLine().getStatusCode());
+            ticket = null;
+        }
+
+        cacheStatus = localContext.get().getAttribute(CachingHttpClient.CACHE_RESPONSE_STATUS);
 //        logger.info("get done, url: {}, status: {}, cacheStatus: {}, headers: {}", new Object[]{url, response.getStatusLine(), null, response.getAllHeaders()});
 //        System.out.println("==> get done, url: "+url+", status: "+response.getStatusLine()+", cacheStatus: "+cacheStatus+", headers: "+ Arrays.asList(response.getAllHeaders()));
 //        System.out.println(IOUtils.toString(response.getEntity().getContent()));
@@ -151,5 +179,41 @@ cacheStatus = localContext.get().getAttribute(CachingHttpClient.CACHE_RESPONSE_S
             logger.warn("error parsing json to xmlgregoriancal: " + string);
         }
         return null;
+    }
+
+    public String getWebCasUrl() {
+        return webCasUrl;
+    }
+
+    public void setWebCasUrl(String webCasUrl) {
+        this.ticket = null;
+        this.webCasUrl = webCasUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.ticket = null;
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.ticket = null;
+        this.password = password;
+    }
+
+    public String getCasService() {
+        return casService;
+    }
+
+    public void setCasService(String casService) {
+        this.ticket = null;
+        this.casService = casService;
     }
 }
