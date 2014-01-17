169fa4d8164ec6e09d4cc4d7e8c2090ffb99fd2e
Antti Salonen <antti.salonen@proactum.fi>
2014-01-17 14:02:35 +0200

    casfilter, don't try to authenticate same ticket header in every request

--- generic-common/src/main/java/fi/vm/sade/security/CustomCasAuthenticationFilter.java
+++ generic-common/src/main/java/fi/vm/sade/security/CustomCasAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package fi.vm.sade.security;
 
 import org.springframework.security.cas.web.CasAuthenticationFilter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,9 +18,18 @@ public class CustomCasAuthenticationFilter extends CasAuthenticationFilter {
 
     @Override
     protected String obtainArtifact(HttpServletRequest request) {
+
+        // ticket-parametrin lisäksi autentikoidaan myös CasSecurityTicket-headerissa oleva ticket
     	String casTicketHeader = request.getHeader(CAS_SECURITY_TICKET);
         if (casTicketHeader != null) {
-            return casTicketHeader;
+
+            // jos ko tiketillä ollaan jo autentikoiduttu sessio, ei tehdä sitä enää
+            if (casTicketHeader.equals(getSessionTicket())) {
+                logger.info("ticket already authenticated in session: "+casTicketHeader);
+                return null;
+            } else {
+                return casTicketHeader;
+            }
         }
 
         // getParameter -kutsu saattaa hajottaa tietyt post-requestit,
@@ -29,6 +40,15 @@ public class CustomCasAuthenticationFilter extends CasAuthenticationFilter {
         }
 
         return super.obtainArtifact(request);
+    }
+
+    private Object getSessionTicket() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            return auth.getCredentials(); // cas ticket is saved as authentication.credentials, if credentials is something else than ticketstring, it doesn't matter
+        } else {
+            return null;
+        }
     }
 
 }
