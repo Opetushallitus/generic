7e4df30e5e0de425521d50407737f90621fc622d
Simo Kosunen <simo.kosunen@cybercom.com>
2014-12-08 15:44:27 +0200

    Cors filterissä lisätään Caller-Id header.

--- generic-common/src/main/java/fi/vm/sade/generic/rest/CorsFiller.java
+++ generic-common/src/main/java/fi/vm/sade/generic/rest/CorsFiller.java
@@ -9,15 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 abstract class CorsFiller<R, Q> {
-    
+
     private final CorsFilterMode mode;
-    
+
     private final String allowedDomains;
-    
+
     private final Logger logger = LoggerFactory.getLogger(getClass());;
-    
+
     protected static final String DEFAULT_DOMAIN_FOR_ALLOW_ORIGIN = "https://virkailija.opintopolku.fi";
-    
+
     protected CorsFiller(CorsFilterMode mode, String allowedDomains) {
         this.mode = mode;
         this.allowedDomains = allowedDomains;
@@ -36,18 +36,18 @@ abstract class CorsFiller<R, Q> {
         List<String> headers = getHeaders("referer", request);
         try {
             if (!headers.isEmpty()) {
-                URL url = new URL(headers.get(0));                
+                URL url = new URL(headers.get(0));
                 return new StringBuilder(url.getProtocol()).append("://").append(url.getHost()).toString();
             }
         } catch (MalformedURLException e) {
             logger.warn("Could not determine domain from request while forming CORS response", e);
-        } 
+        }
         return DEFAULT_DOMAIN_FOR_ALLOW_ORIGIN;
     }
 
     protected void setAllowOrigin(R response, Q request) {
         if (CorsFilterMode.DEVELOPMENT.equals(mode)) {
-            setHeader("Access-Control-Allow-Origin", getRemoteDomain(request), response);           
+            setHeader("Access-Control-Allow-Origin", getRemoteDomain(request), response);
         } else if(StringUtils.isNotBlank(allowedDomains)){
             String matchingDomain = getMatchingDomain(request);
             if (StringUtils.isNotBlank(matchingDomain)) {
@@ -60,6 +60,7 @@ abstract class CorsFiller<R, Q> {
         for (String value : getHeaders("access-control-request-headers", request)) {
             setHeader("Access-Control-Allow-Headers", value, response);
         }
+        setHeader("Access-Control-Allow-Headers", "Caller-Id", response);
         setHeader("Access-Control-Allow-Credentials", "true", response);
         setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD", response);
         setHeader("Access-Control-Max-Age", "604800", response);
