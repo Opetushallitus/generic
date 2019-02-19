92d75063290488a77aa6d71a6e43c83f968c84fe
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2019-02-19 15:35:41 +0200

    Clean up source code
    No functional changes

--- generic-common/src/main/java/fi/vm/sade/authentication/business/service/Authorizer.java
+++ generic-common/src/main/java/fi/vm/sade/authentication/business/service/Authorizer.java
@@ -1,10 +1,9 @@
 package fi.vm.sade.authentication.business.service;
 
-import fi.vm.sade.generic.common.auth.Role;
 import fi.vm.sade.generic.service.exception.NotAuthorizedException;
 
 public interface Authorizer {
-    public void checkUserIsNotSame(String userOid) throws NotAuthorizedException;
+    void checkUserIsNotSame(String userOid) throws NotAuthorizedException;
 
-    public void checkOrganisationAccess(String targetOrganisationOid, String... roles) throws NotAuthorizedException;
+    void checkOrganisationAccess(String targetOrganisationOid, String... roles) throws NotAuthorizedException;
 }
